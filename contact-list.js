98adb1c8a828a371e3d451e52befe4ad19720dbd
Jose Sosa <josesosa535@gmail.com>
2023-04-19 01:01:21 -0500

    se agregaron objetos a la lista de contactos y se modificaron las funciones

--- contact-list.js
+++ contact-list.js
@@ -2,31 +2,51 @@ let listaContactos = [];
 listaContactos = [
   {
     id: 1,
-    nombre: "Yessica Sotomayor",
+    nombre: "Yessica",
+    apellido: "Sotomayor",
     telefono: 3023423212,
+    ubicaciones: {
+      ciudad: "Cali",
+      direccion: "av 5 oeste #19a-36 apto 201",
+    },
   },
   {
     id: 2,
-    nombre: "Jose Sosa",
+    nombre: "Jose",
+    apellido: "Sosa",
     telefono: 3023630601,
+    ubicaciones: {
+      ciudad: "Barrancabermeja",
+      direccion: "diag 48 #25-92",
+    },
   },
   {
     id: 3,
-    nombre: "Martin Morales",
+    nombre: "Martin",
+    apellido: "Morales",
     telefono: 3125659966,
+    ubicaciones: {
+      ciudad: "Bucaramanga",
+      direccion: "cra 36 #23-41",
+    },
   },
 ];
-function agregarContacto(nombre, telefono) {
+function agregarContacto(nombre, apellido, telefono, ciudad, direccion) {
   var contacto = {
     id: listaContactos[listaContactos.length - 1].id + 1,
     nombre: nombre,
     telefono: telefono,
+    apellido: apellido,
+    ubicaciones: {
+      ciudad: ciudad,
+      direccion: direccion,
+    },
   };
   listaContactos.push(contacto);
 }
 
-agregarContacto("Said Sosa", "3024440601");
-agregarContacto("Astrid Sosa", "3025550602");
+agregarContacto("Said", "Sosa", "3024440601", "Yondo", "cra 7 #3-4");
+agregarContacto("Astrid", "Sosa", "3025550602", "Bogota", "cra 103 #72-70");
 
 const eliminarContacto = (nombre) => {
   for (let contactos of listaContactos) {
@@ -43,7 +63,10 @@ function mostrarContactos() {
     let contacto = listaContactos[i];
     console.log("id: " + contacto.id);
     console.log("Nombre: " + contacto.nombre);
+    console.log("Apellido: " + contacto.apellido);
     console.log("Telefono: " + contacto.telefono);
+    console.log("Ciudad: " + contacto.ubicaciones.ciudad);
+    console.log("Direccion: " + contacto.ubicaciones.direccion);
     console.log("------------");
   }
 }
